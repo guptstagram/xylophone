14be60e86ac608dcadc3346111590fff9ac597f9
guptstagram <sourabh.gupta.5095@gmail.com>
2020-08-08 16:56:22 +0530

    xylophone

---
+++ script.js
@@ -0,0 +1,42 @@
+const playNote1=()=>{
+    var note1=document.querySelector("#note1");
+    note1.preload = 'auto';
+    note1.load();
+    note1.play();
+}
+const playNote2=()=>{
+    var note2=document.querySelector("#note2");
+    note2.preload = 'auto';
+    note2.load();
+    note2.play();
+}
+const playNote3=()=>{
+    var note3=document.querySelector("#note3");
+    note3.preload = 'auto';
+    note3.load();
+    note3.play();
+}
+const playNote4=()=>{
+    var note4=document.querySelector("#note4");
+    note4.preload = 'auto';
+    note4.load();
+    note4.play();
+}
+const playNote5=()=>{
+    var note5=document.querySelector("#note5");
+    note5.preload = 'auto';
+    note5.load();
+    note5.play();
+}
+const playNote6=()=>{
+    var note6=document.querySelector("#note6");
+    note6.preload = 'auto';
+    note6.load();
+    note6.play();
+}
+const playNote7=()=>{
+    var note7=document.querySelector("#note7");
+    note7.preload = 'auto';
+    note7.load();
+    note7.play();
+}
